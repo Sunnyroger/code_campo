2e4d86e03fa86f3befd8a31337e6942166448bc3
Sunny <sunny@sunny.(none)>
2013-04-09 21:33:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeCampo::Application.config.secret_token = 'd39b1482b29ba6e423c4b5188ea1825997d1f88f7cacea2a2fe1c81fee6a6ac519cf1f5bbd13d54e690ca834a8a853cb93fd2c90f60a2283e5ac6dda97aa759a'
